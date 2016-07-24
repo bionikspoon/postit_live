4c755428ec66fee29e2107e9baa54467bcd47ae5
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-07-23 19:00:06 -0500

    use CDN_PATH

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,9 @@
 const OPTIONS = {
   PROJECT_ROOT: __dirname,
   NODE_ENV: process.env.NODE_ENV,
-  CDN_PATH: process.env.CDN_PATH,
+  CDN_PATH: process.env.DJANGO_AWS_STORAGE_BUCKET_NAME
+    ? `https://s3.amazonaws.com:443/${process.env.DJANGO_AWS_STORAGE_BUCKET_NAME}/static/`
+    : undefined,
 };
 
 module.exports = (() => {
