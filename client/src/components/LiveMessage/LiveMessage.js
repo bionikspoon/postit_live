193d63451ff4e83715b74942cd32fbd30369b44c
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-07-29 03:09:04 -0500

    Create Moment time component

--- client/src/components/LiveMessage/LiveMessage.js
+++ client/src/components/LiveMessage/LiveMessage.js
@@ -1,13 +1,12 @@
 import './LiveMessage.scss';
 import React, { PropTypes, Component } from 'react';
+import Moment from '../Moment';
 
 export default function LiveMessage({ created, body, author }) {
   return (
     <div className="row">
       <div className="col-md-2">
-        <a href="#">
-          <time dateTime={created} title={created}>{created}</time>
-        </a>
+        <Moment date={created} href="#" />
       </div>
       <div className="col-md-10"><span>{body}</span>
         <a href="#" className="author">/u/{author}</a>
---
+++ client/src/components/Moment/Moment.js
@@ -0,0 +1,51 @@
+import './Moment.scss';
+import React, { PropTypes, Component } from 'react';
+import moment from 'moment';
+
+export default class Moment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = createDate(this.props.date);
+  }
+
+  componentWillMount() {
+    const intervalId = setInterval(() => {
+      this.setState({ fromNow: this.state.date.fromNow() });
+    }, 10 * 1000);
+    this.setState({ intervalId });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.date !== nextProps.date
+      || this.props.href !== nextProps.href
+      || this.state.fromNow !== nextState.fromNow;
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  render() {
+    const { title, iso, fromNow } = this.state;
+    const { href } = this.props;
+    return (
+      <a href={href}>
+        <time dateTime={iso} title={title}>{fromNow}</time>
+      </a>
+    );
+  }
+}
+Moment.propTypes = {
+  date: PropTypes.number.isRequired,
+  href: PropTypes.string.isRequired,
+};
+
+function createDate(epoch) {
+  const date = moment.unix(epoch);
+  return {
+    date,
+    title: date.format('LLLL'),
+    iso: date.toISOString(),
+    fromNow: date.fromNow(),
+  };
+}
