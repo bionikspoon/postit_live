12b1da13668cf6741f9382a508272c3dde7d1cd8
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-08-01 20:40:31 -0500

    refactor receive channel to an independant channel

--- config/routing.py
+++ config/routing.py
@@ -1,7 +1,11 @@
 # coding=utf-8
 from channels import include
+from channels import route
+
+from postit_live.live import consumers
 
 channel_routing = [
     include('postit_live.chat.routing.channel_routing', path=r'^/chat'),
     include('postit_live.live.routing.channel_routing', path=r'^/live'),
+    route('live-messages', consumers.live_messages_consumer),
 ]
--- postit_live/live/consumers.py
+++ postit_live/live/consumers.py
@@ -1,6 +1,7 @@
 import json
 import logging
 
+import channels
 from channels import Group
 from channels.sessions import channel_session
 from rest_framework.renderers import JSONRenderer
@@ -11,7 +12,48 @@ from .models import Channel
 
 logger = logging.getLogger(__name__)
 
-render = JSONRenderer()
+to_json = JSONRenderer()
+
+CREATE = 'socket.CREATE'
+STRIKE = 'socket.STRIKE'
+DELETE = 'socket.DELETE'
+
+
+def live_messages_consumer(message):
+    try:
+        slug = message.content['slug']
+        data = json.loads(message.content['text'])
+        if set(data.keys()) != {'type', 'payload'}:
+            raise KeyError
+    except KeyError:
+        logger.error('live_messages message is malformed')
+        return
+
+    try:
+        group = Group('live-%s' % slug)
+        channel = Channel.objects.get(slug=slug)
+    except Channel.DoesNotExist:
+        logger.error('message received, but channel does not exist slug=%s', slug)
+        return
+
+    if data['type'] == CREATE:
+        return create_message(data['payload']['body'], channel, group)
+
+
+def create_message(body, channel, group):
+    message = channel.messages.create(body=body, author=User.objects.all().first())
+    serializer = MessageSocketSerializer(message)
+
+    data = {
+        'type': 'live.CREATE',
+        'payload': {
+            'message': serializer.data
+        }
+    }
+
+    text = to_json.render(data).decode('utf-8')
+    group.send({'text': text})
+    logger.debug('message created, text=%s' % text)
 
 
 @channel_session
@@ -34,61 +76,27 @@ def ws_connect(message, slug):
 def ws_receive(message):
     try:
         slug = message.channel_session['slug']
+        text = message['text']
     except KeyError:
-        logger.error('no "slug" in channel_session')
+        logger.error('message is malformed')
         return
 
-    group = Group('live-%s' % slug)
-    logger.debug('receive group=%s', group)
-
-    try:
-        channel = Channel.objects.get(slug=slug)
-    except Channel.DoesNotExist:
-        logger.error('message received, but channel does not exist slug=%s', slug)
-        return
-
-    try:
-        data = json.loads(message['text'])
-    except ValueError:
-        logger.error('could not deserialize text=%s', message['text'])
-        return
-
-    if set(data.keys()) != {'type', 'payload'}:
-        logger.error('message data malformed data=%s', data)
-
-    response = {
-        'type': data['type'].replace('socket', 'live', 1),
-        'payload': {},
-    }
-
-    if response['type'] == CREATE:
-        logger.debug('response.type=%s data.type=%s', response['type'], data['type'])
-
-        # TODO, user current user
-        message_obj = channel.messages.create(body=data['payload']['body'], author=User.objects.all().first())
-        serializer = MessageSocketSerializer(message_obj)
-        response['payload']['message'] = serializer.data
-
-        response_text = render.render(response).decode("utf-8")
-        group.send({'text': response_text})
-        logger.debug('message sent slug=%s text=%s' % (slug, response_text))
+    channel = channels.Channel('live-messages')
+    logger.debug('channel=%s', channel)
+    return channel.send({
+        'slug': slug,
+        'text': text,
+    })
 
 
 @channel_session
 def ws_disconnect(message, slug):
     try:
-        channel = Channel.objects.get(slug=slug)
+        Channel.objects.get(slug=slug)
     except Channel.DoesNotExist:
         logger.error('channel disconnect failed, channel does not exist slug=%s', slug)
         return
 
     group = Group('live-%s' % slug)
-    logger.debug('connect group=%s', group)
-
     group.discard(message.reply_channel)
     logger.debug('disconnected slug=%s', slug)
-
-
-CREATE = 'live.CREATE'
-STRIKE = 'live.STRIKE'
-DELETE = 'live.DELETE'
