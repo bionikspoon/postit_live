b78564cefd928ba802a0c9c64eb2d524e9987067
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-08-02 11:21:38 -0500

    gcm refactor socket

--- client/src/middleware/socket.js
+++ client/src/middleware/socket.js
@@ -1,64 +1,19 @@
 import * as types from '../constants/SocketActionTypes';
+import Socket from '../utils/socket';
 
-class Socket {
-  constructor(store, location) {
-    const { dispatch } = store;
-    const { protocol, host, pathname } = location;
-
-    const scheme = protocol === 'https:' ? 'wss:' : 'ws:';
-    this.dispatch = dispatch;
-    this.path = `${scheme}//${host}${pathname}`;
-    this.conn = null;
-
-    this.open = this.open.bind(this);
-    this.send = this.send.bind(this);
-    this.onopen = this.onopen.bind(this);
-    this.onmessage = this.onmessage.bind(this);
-    this.onclose = this.onclose.bind(this);
-    this.onerror = this.onerror.bind(this);
-  }
-
-  open() {
-    console.debug('open this=', this);
-    const conn = new WebSocket(this.path);
-    conn.onopen = this.onopen;
-    conn.onmessage = this.onmessage;
-    conn.onclose = this.onclose;
-    conn.onerror = this.onerror;
-    this.conn = conn;
-  }
-
-  send(data) {
-    console.debug('send data=', data);
-    this.conn.send(data);
-  }
-
-  onopen(event) {
-    console.debug('onopen event=', event);
-  }
-
+class LiveSocket extends Socket {
   onmessage(event) {
-    console.debug('onmessage event=', event);
+    super.onmessage(event);
 
-    const data = JSON.parse(event.data);
-    console.debug('onmessage data=', data);
-    this.dispatch(data);
-  }
+    const action = JSON.parse(event.data);
+    this.dispatch(action);
 
-  onclose(event) {
-    console.debug('onclose event=', event);
-
-    this.conn = null;
-    if (!event.wasClean) setTimeout(this.open, 500);
-  }
-
-  onerror(event) {
-    console.debug('onerror event=', event);
+    console.debug('onmessage action=', action);
   }
 }
 
 export default function connect(store) {
-  const socket = new Socket(store, window.location);
+  const socket = new LiveSocket(store, window.location);
   const middlewareActions = Object.keys(types).map(type => types[type]);
 
   socket.open();
---
+++ client/src/utils/socket.js
@@ -0,0 +1,56 @@
+/* eslint no-console:0 */
+export default class Socket {
+
+  constructor(store, location, timeout = 500) {
+    const { dispatch } = store;
+    const { protocol, host, pathname } = location;
+
+    const scheme = protocol === 'https:' ? 'wss:' : 'ws:';
+    this.dispatch = dispatch;
+    this.path = `${scheme}//${host}${pathname}`;
+    this.conn = null;
+    this.timeout = timeout;
+
+    this.open = this.open.bind(this);
+    this.send = this.send.bind(this);
+    this.onopen = this.onopen.bind(this);
+    this.onmessage = this.onmessage.bind(this);
+    this.onclose = this.onclose.bind(this);
+    this.onerror = this.onerror.bind(this);
+  }
+
+  open() {
+    console.debug('open this=', this);
+    const conn = new WebSocket(this.path);
+
+    conn.onopen = this.onopen;
+    conn.onmessage = this.onmessage;
+    conn.onclose = this.onclose;
+    conn.onerror = this.onerror;
+    this.conn = conn;
+  }
+
+  send(data) {
+    console.debug('send data=', data);
+    this.conn.send(data);
+  }
+
+  onopen(event) {
+    console.debug('onopen event=', event);
+  }
+
+  onmessage(event) {
+    console.debug('onmessage event=', event);
+  }
+
+  onclose(event) {
+    console.debug('onclose event=', event);
+
+    this.conn = null;
+    if (!event.wasClean) setTimeout(this.open, this.timeout);
+  }
+
+  onerror(event) {
+    console.debug('onerror event=', event);
+  }
+}
