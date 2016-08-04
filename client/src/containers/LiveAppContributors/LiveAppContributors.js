a9efbcfb35ec9bf3d5bf745de70f254f5f57cd50
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-08-03 23:01:32 -0500

    scaffold contributors page

--- client/src/containers/LiveAppContributors/LiveAppContributors.js
+++ client/src/containers/LiveAppContributors/LiveAppContributors.js
@@ -13,6 +13,70 @@ export class LiveAppContributors extends Component {
 
         <LayoutInnerRow>
           <h1>Contributors</h1>
+          <div className="alert alert-warning">
+            you are a contributor to this live channel. |
+            <a href="#">leave</a>
+          </div>
+
+          <div>
+            <h2>current contributors</h2>
+            <table>
+              <tbody>
+                <tr>
+                  <td>/u/user</td>
+                  <td>
+                    <a href="#">remove</a>
+                  </td>
+                  <td>full permissions</td>
+                  <td>(
+                    <a href="#">change</a>
+                      )
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div>
+            <h2>invite contributor</h2>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <input type="text" />
+                  </td>
+                  <td>full permissions(
+                    <a href="#">change</a>
+                      )
+                  </td>
+                  <td>
+                    <button>invite</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+
+          <div>
+            <h2>invited contributors</h2>
+            <table>
+              <tbody>
+                <tr>
+                  <td>/u/admin</td>
+                  <td>
+                    <a href="#">remove</a>
+                  </td>
+                  <td>full permissions</td>
+                  <td>(
+                    <a href="#">change</a>
+                      )
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
         </LayoutInnerRow>
 
       </LayoutRow>
