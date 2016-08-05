85d30b632a7ec341e1b9436005424f90649d0c85
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-08-04 19:05:03 -0500

    install guardian

--- config/routing.py
+++ config/routing.py
@@ -2,6 +2,5 @@
 from channels import include
 
 channel_routing = [
-    # include('postit_live.chat.routing.channel_routing', path=r'^/chat'),
     include('postit_live.live.routing.channel_routing'),
 ]
