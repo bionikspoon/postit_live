6eb9f44c547e29b89ff2c2fc4e6e7564918e2ba4
Manu Phatak (bionikspoon) <bionikspoon@gmail.com>
2016-08-06 14:30:00 -0500

    register users

--- postit_live/user/forms.py
+++ postit_live/user/forms.py
@@ -1,8 +1,28 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class UserAuthenticationForm(AuthenticationForm):
     def __init__(self, request=None, *args, **kwargs):
         super().__init__(request, *args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'form-control'})
-        self.fields['password'].widget.attrs.update({'class': 'form-control'})
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'required': True})
+        self.fields['password'].widget.attrs.update({'class': 'form-control', 'required': True})
+
+
+class UserRegistrationForm(UserCreationForm):
+    def __init__(self, request=None, **kwargs):
+        super().__init__(**kwargs)
+        self.request = request
+
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'required': True})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'required': True})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'required': True})
+
+    def save(self, commit=True, login=False):
+        user = super().save(commit=commit)
+
+        if commit and login:
+            user = auth.authenticate(username=self.cleaned_data['username'], password=self.cleaned_data['password1'])
+            auth.login(self.request, user)
+
+        return user
--- postit_live/user/views.py
+++ postit_live/user/views.py
@@ -1,13 +1,21 @@
 import logging
 
-from django.contrib.auth import views, get_user_model
+from django.conf import settings
+from django.contrib import auth
+from django.contrib.auth import views, get_user_model, REDIRECT_FIELD_NAME
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect, resolve_url
+from django.utils.http import is_safe_url
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
 from guardian.shortcuts import get_perms
 from rest_framework import viewsets
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
 
 from postit_live.live.models import LiveChannel
-from .forms import UserAuthenticationForm
+from .forms import UserAuthenticationForm, UserRegistrationForm
 from .serializers import UserSerializer
 
 logger = logging.getLogger(__name__)
@@ -15,11 +23,42 @@ User = get_user_model()
 
 
 def login(request):
-    return views.login(request, template_name='user/login.html', authentication_form=UserAuthenticationForm)
+    registration_form = UserRegistrationForm(request=request, data=request.session.pop('registration_form', None))
+
+    return views.login(
+        request,
+        template_name='user/login.html',
+        authentication_form=UserAuthenticationForm,
+        extra_context={'registration_form': registration_form}
+    )
 
 
 def logout(request):
-    return views.logout(request)
+    auth.logout(request)
+    return redirect('home')
+
+
+@sensitive_post_parameters()
+@csrf_protect
+@never_cache
+def register(request):
+    redirect_to = request.POST.get(REDIRECT_FIELD_NAME, request.GET.get(REDIRECT_FIELD_NAME, ''))
+
+    if request.method != 'POST':
+        query = '?next=%s' % redirect_to if redirect_to else ''
+        return redirect(reverse('user:login') + query)
+
+    form = UserRegistrationForm(request, data=request.POST)
+    if not form.is_valid():
+        request.session['registration_form'] = request.POST
+        return redirect(reverse('user:login'))
+
+    if not is_safe_url(url=redirect_to, host=request.get_host()):
+        redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
+
+    user = form.save(login=True)
+    logger.info('user created and logged in user=%s', user)
+    return redirect(redirect_to)
 
 
 def password_change(request):
